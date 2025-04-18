c15112a2923ba3e717159ad3daef955f2f0004ea
urish11 <117926571+urish11@users.noreply.github.com>
2025-04-18 15:31:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,6 @@ def search_youtube(api_key, query, max_results=5):
             
         try:
             response = requests.get(YOUTUBE_API_BASE_URL, params=params, timeout=15) # Increased timeout
-            input()
 
             response.raise_for_status() # Raises HTTPError for bad responses (4xx or 5xx)
             results = response.json()
