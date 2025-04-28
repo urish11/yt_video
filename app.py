54cfcc671ce59e011eacb044261432413408d47c
urish11 <117926571+urish11@users.noreply.github.com>
2025-04-28 12:09:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -860,7 +860,7 @@ def download_direct_url(url, suffix=".mp4"):
     """
     local_path = None
     try:
-      headers = {
+        headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
     }
 
