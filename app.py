81eb5ca67a173c253c8ed4b113cf0d7102d877c1
urish11 <uri1234@gmail.com>
2025-09-28 17:38:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2552,15 +2552,15 @@ if st.session_state.api_search_results:
                                           # st.image(thumbnail, caption="Video Thumbnail",width = "stretch")
                                           st.markdown(
                                                             f"""
-<div style="width:220px; height:280px; 
-            border-radius:8px; background:#111; 
-            overflow:hidden; text-align:center;">
+                                                                <div style="width:220px; height:320px; 
+                                                                            border-radius:8px; background:#111; 
+                                                                            overflow:hidden; text-align:center;">
 
-  <img src="{thumbnail_url}" alt="Video Thumbnail" 
-       style="width:100%; height:100%; object-fit:cover; 
-              border-radius:6px; display:block;"/>
+                                                                <img src="{thumbnail_url}" alt="Video Thumbnail" 
+                                                                    style="width:100%; height:100%; object-fit:cover; 
+                                                                            border-radius:6px; display:block;"/>
 
-</div>
+                                                                </div>
                                                             """,
                                                             unsafe_allow_html=True) 
 
